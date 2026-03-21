ba950386fd6d36fa6c644dcc7b4eba6981913f99
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-21 20:04:01 -0300

    remove unused exception

--- src/main/java/br/com/fiap/tech_challenge_ii/starter/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.tech_challenge_ii.starter.exception;
-
-public class InvalidCredentialsException extends SystemBaseException {
-
-    public InvalidCredentialsException() {
-        super("INVALID_CREDENTIALS", "Invalid login or password", 401);
-    }
-}
