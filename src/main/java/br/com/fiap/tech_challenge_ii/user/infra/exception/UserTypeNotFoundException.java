b9ea1a14607be7d548b6086dadf200c42d47f0cb
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-24 23:37:16 -0300

    fix

--- src/main/java/br/com/fiap/tech_challenge_ii/user/infra/exception/UserTypeNotFoundException.java
+++ src/main/java/br/com/fiap/tech_challenge_ii/user/infra/exception/UserTypeNotFoundException.java
@@ -1,20 +1,19 @@
 package br.com.fiap.tech_challenge_ii.user.infra.exception;
 
-
-import br.com.fiap.tech_challenge_ii.user.core.exception.SystemBaseException;
-
 import java.io.Serial;
 
+import br.com.fiap.tech_challenge_ii.starter.exception.SystemBaseException;
+
 public class UserTypeNotFoundException extends SystemBaseException {
-	@Serial
+    @Serial
     private static final long serialVersionUID = 756435066530793530L;
-	
-	private static final String CODE = "usuario.tipoUsuarioNaoExiste";
-	private static final String MESSAGE = "Tipo de usuário não existe";
-	private static final Integer HTTP_STATUS = 422;
-	
-	public UserTypeNotFoundException() {
-		super(CODE, MESSAGE, HTTP_STATUS);
-	}
-	
+
+    private static final String CODE = "usuario.tipoUsuarioNaoExiste";
+    private static final String MESSAGE = "Tipo de usuário não existe";
+    private static final Integer HTTP_STATUS = 422;
+
+    public UserTypeNotFoundException() {
+        super(CODE, MESSAGE, HTTP_STATUS);
+    }
+
 }
